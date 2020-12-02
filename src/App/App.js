481ad7756e972571c2ab5ae67988f11095d0fdaa
Bryce Mara Jarrett <brycej98@gmail.com>
2020-12-02 15:42:04 -0700

    Add single display render

--- src/App/App.js
+++ src/App/App.js
@@ -2,27 +2,33 @@ import React, { Component } from 'react'
 import './App.css';
 import movieData from '../temp-data.js'
 import Movies from '../Movies/Movies.js'
+import SingleMovie from '../SingleMovie/SingleMovie.js'
 
 class App extends Component {
   constructor() {
     super()
     this.state = { 
       movies : movieData.movies,
-      currentMovie : undefined
+      currentMovie : undefined,
      }
   }
 
   updateCurrentMovie = (id) => {
     const clickedMovie = this.state.movies.find(movie => movie.id === id);
     this.setState({currentMovie: clickedMovie})
-    
+
   }
 
   render() {
     return (
     <main className="App">
       <h1>Rotten Tomatoes Rip-Off</h1>
-      <Movies movies={ this.state.movies } updateCurrentMovie={ this.updateCurrentMovie } />
+      {this.state.currentMovie && 
+        <SingleMovie movie={ this.state.currentMovie }/>
+      }
+      {!this.state.currentMovie &&
+        <Movies movies={ this.state.movies } updateCurrentMovie={ this.updateCurrentMovie } />
+      }
     </main>
   )}
 }
--- src/SingleMovie/SingleMovie.js
+++ src/SingleMovie/SingleMovie.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 
 
 class SingleMovie extends Component {
-    constructor() {
-        super()
-        this.state = {}
+    constructor(movie) {
+        super(movie)
+        this.state = {movie: movie}
     }
 
     render() {
+        return(
         <section className="single-movie">
-            <h2>{}</h2>
-            <img src=""/>
+            <h2>{this.state.title}</h2>
+            <img src="" alt="single-movie"/>
             <p>overview</p>
             <p>rating</p>
             <p>genre</p>
             <p>rev</p>
         </section>
+        )
     }
 }
+
+export default SingleMovie;
