5cd067f7a9c67023d3f822434afc3649a29bd6f4
Bryce Mara Jarrett <brycej98@gmail.com>
2020-12-02 15:27:05 -0700

    Add current movie functionality

---
+++ src/SingleMovie/SingleMovie.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class SingleMovie extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        <section className="single-movie">
+            <h2>{}</h2>
+            <img src=""/>
+            <p>overview</p>
+            <p>rating</p>
+            <p>genre</p>
+            <p>rev</p>
+        </section>
+    }
+}
