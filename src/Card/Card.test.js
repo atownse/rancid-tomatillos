f5e44d204fa4dcd90709ca2ed580ba8018485801
Bryce Mara Jarrett <brycej98@gmail.com>
2020-12-09 16:10:13 -0700

    Update testing for Router

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,44 +1,29 @@
 import React from 'react'
 import Card from './Card.js'
-import { render, screen, fireEvent} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
 
-describe.only('Card', () => {
+
+describe('Card', () => {
   it('should render the correct movie content', () => {
-    render(<Card
-      id = { 420 }
-      posterPath = "absolute garbage"
-      backdropPath = "supoosed to be a link"
-      title = "Random Movie"
-      averageRating = { 9 }
-      releaseDate =  "The twelfth of never"
-      key = { 420 }
-      updateCurrentMovie = { jest.fn() }
-      />)
-
+    render(
+      <MemoryRouter>
+        <Card
+          id = { 420 }
+          posterPath = "absolute garbage"
+          backdropPath = "supoosed to be a link"
+          title = "Random Movie"
+          averageRating = { 9 }
+          releaseDate =  "The twelfth of never"
+          key = { 420 }
+          />
+      </MemoryRouter>
+      )
       const title = screen.getByText("Random Movie")
       const rating = screen.getByText("Average Rating: 90%")
 
       expect(title).toBeInTheDocument()
       expect(rating).toBeInTheDocument()
-  })
-
-  it('should render the correct movie content', () => {
-    const mockUpdateCurrentMovie = jest.fn()
-    render(<Card
-      id = { 420 }
-      posterPath = "absolute garbage"
-      backdropPath = "supoosed to be a link"
-      title = "Random Movie"
-      averageRating = "11/10"
-      releaseDate =  "The twelfth of never"
-      key = { 420 }
-      updateCurrentMovie = { mockUpdateCurrentMovie }
-      />)
-
-      const movieCard = screen.getByTitle("Random Movie")
-      fireEvent.click(movieCard)
-
-      expect(mockUpdateCurrentMovie).toHaveBeenCalledWith(420)
   })
 })
